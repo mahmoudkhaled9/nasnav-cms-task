7a629fff70a140da6c3ca48d1a38f4190d6b10e5
Mahmoud khaled <mmahmoud@must.edu>
2018-09-16 07:02:33 +0200

    nasnav task

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule , Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import {UsersService} from './users.service';
+import { HomeComponent } from './home/home.component';
+import { BusinessComponent } from './business/business.component';
+import { TechComponent } from './tech/tech.component';
+import { SportComponent } from './sport/sport.component';
+
+const appRoutes:Routes = [
+  {
+    path:'home',
+    component:HomeComponent,
+  },
+  {
+    path:'business',
+    component:BusinessComponent,
+  },
+  {
+    path:'tech',
+    component:TechComponent,
+  },
+  {
+    path:'sport',
+    component:SportComponent,
+  },
+  {
+    path:'gallary',
+    component:GallaryComponent,
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GallaryComponent,
+    HomeComponent,
+    BusinessComponent,
+    TechComponent,
+    SportComponent,
+  ],
+  imports: [
+    BrowserModule,HttpModule,RouterModule.forRoot(appRoutes)
+  ],
+  providers: [UsersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gallary/gallary.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../users.service'
+
+@Component({
+  selector: 'app-gallary',
+  templateUrl: './gallary.component.html',
+  styleUrls: ['./gallary.component.css']
+})
+export class GallaryComponent implements OnInit {
+
+
+
+  ngOnInit() {
+  }
+
+}
